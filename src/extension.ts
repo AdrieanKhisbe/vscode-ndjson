7f8df2cec178f3c56660b6df860b720b835960c9
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-11-05 22:40:21 +0100

    Set up gutter icons for errors :vertical_traffic_light:

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,22 @@
 import * as vscode from 'vscode';
 
 let diagnosticCollection: vscode.DiagnosticCollection;
+let ndjsonErrorDecorator: vscode.TextEditorDecorationType;
 
-export function checkDiagnostics(document: vscode.TextDocument): void {
+export function checkDiagnostics(editor: vscode.TextEditor): void {
   diagnosticCollection.clear();
 
+  const document = editor.document;
   const jsons = document.getText().split('\n');
   const errors = [];
+  const errorDecorations = [];
   jsons.forEach((json, index) => {
     if (/^\s*$/.test(json)) return;
     try {
       JSON.parse(json);
     } catch (err) {
       errors.push([index, err.message]);
+      errorDecorations.push(document.lineAt(index));
     }
   });
   const diagnostics = errors.map(([index, err]) => {
@@ -24,31 +28,39 @@ export function checkDiagnostics(document: vscode.TextDocument): void {
     );
   });
   diagnosticCollection.set(document.uri, diagnostics);
+  editor.setDecorations(ndjsonErrorDecorator, errorDecorations);
 }
 
 const runDiagnostics = (editor: vscode.TextEditor): void => {
   if (!editor || editor.document.languageId !== 'ndjson') return;
 
-  checkDiagnostics(editor.document);
+  checkDiagnostics(editor);
 };
 
 export function activate(context: vscode.ExtensionContext): void {
   diagnosticCollection = vscode.languages.createDiagnosticCollection('ndjson');
   context.subscriptions.push(diagnosticCollection);
 
-  vscode.window.onDidChangeActiveTextEditor(
-    runDiagnostics,
-      null,
-      context.subscriptions
+  const gutterIconSize = '75%'; // ¤note: ou “contain”
+  const opacity = '0.9'; // §TODO: future use, toggle opacity, between mean and max
+  ndjsonErrorDecorator = vscode.window.createTextEditorDecorationType({
+    dark: {
+      gutterIconPath: context.asAbsolutePath('resources/dark/ndjson-error.svg'),
+      gutterIconSize,
+      opacity
+    },
+    light: {
+      gutterIconPath: context.asAbsolutePath('resources/light/ndjson-error.svg'),
+      gutterIconSize,
+      opacity
+    }
+  });
+
+  context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(runDiagnostics));
+  context.subscriptions.push(
+    vscode.window.onDidChangeTextEditorSelection(event => runDiagnostics(event.textEditor))
   );
-  vscode.window.onDidChangeTextEditorSelection(
-    event => runDiagnostics(event.textEditor),
-      null,
-      context.subscriptions
-  );
-  vscode.workspace.onDidChangeTextDocument(
-      () => runDiagnostics(vscode.window.activeTextEditor),
-      null,
-      context.subscriptions
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument(() => runDiagnostics(vscode.window.activeTextEditor))
   );
 }
