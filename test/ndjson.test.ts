c63d26ef2ebdfc47eb79d4b7fa13882a1490760e
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-11-05 19:34:52 +0100

    Tweak lint and import describe :wrench:

--- test/ndjson.test.ts
+++ test/ndjson.test.ts
@@ -1,10 +1,10 @@
-/* global decribe, it */
 import {checkDiagnostics} from '../src/extension';
+import {describe, it} from 'mocha';
 import {expect} from 'chai';
 
 // Defines a Mocha test suite to group tests of similar kind together
 describe('So far dummy tests', function() {
-    it('that just check function exposed', () => {
-        expect(checkDiagnostics).to.be.a('function');
-    })
+  it('that just check function exposed', () => {
+    expect(checkDiagnostics).to.be.a('function');
+  });
 });
