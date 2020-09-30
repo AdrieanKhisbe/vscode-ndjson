0b9425c996602ebe9935964e808c9c464c3b6b56
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-09-30 20:41:02 +0200

    Update eslint and related, and apply :police_officer:

--- test/ndjson.test.ts
+++ test/ndjson.test.ts
@@ -1,9 +1,9 @@
-import {checkDiagnostics} from '../src/extension';
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
+import {checkDiagnostics} from '../src/extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-describe('So far dummy tests', function() {
+describe('So far dummy tests', function () {
   it('that just check function exposed', () => {
     expect(checkDiagnostics).to.be.a('function');
   });
